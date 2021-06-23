25373a62ab47732604729932fbdfff395b759c0e
Mattia Campagna <me@mattiacampagna.com>
2021-06-23 16:17:06 +0200

    .env

--- src/utils/AxiosCaller.js
+++ src/utils/AxiosCaller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosCaller = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
 
