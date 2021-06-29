9b039aeb1f73a1e7ceddb0b62afea8a58d33d556
Mattia Campagna <me@mattiacampagna.com>
2021-06-29 15:26:31 +0200

    Minor improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router";
 import ConsentPage from "./pages/Consent/ConsentPage";
 import Home from "./pages/Home/Home"
 import LoginPage from "./pages/Login/LoginPage"
+import './App.less'
 
 const App = () => {
   return (
